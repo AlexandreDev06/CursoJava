5a7541a7ef40ae132b457aeb4232f7c63d507964
alexandre <alexandre.macedo1712@gmail.com>
2020-12-11 00:02:56 -0300

    Att aula dia 10/12

--- 8_Intrudução_poo/Classes/src/application/Program.java
+++ 8_Intrudução_poo/Classes/src/application/Program.java
@@ -10,26 +10,27 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		Calculator calc = new Calculator();
+		//Calculator calc = new Calculator();
 
 		System.out.print("Enter radius: ");
 		double radius = sc.nextDouble();
 
-		double c = calc.circumFerence(radius);
-
-		double v = calc.volume(radius);
+		double c = Calculator.circumFerence(radius);// metodo chamado de forma estatica
+		//double c = calc.circumFerence(radius);//metodo de forma instanciada
+		
+		double v = Calculator.volume(radius);// metodo chamado de forma estatica
 
 		System.out.printf("Circumference: %.2f%n", c);
 		System.out.printf("Volume: %.2f%n", v);
-		System.out.printf("PI value: %.2f%n", calc.PI);
+		System.out.printf("PI value: %.2f%n", Calculator.PI);// metodo chamado de forma estatica
 
 		sc.close();
 
 	}
 
 }
-/*PARTE 1 -  SEM CLASSES
- * public class Program {
+/*
+ * PARTE 1 - SEM CLASSES public class Program {
  * 
  * public static final double PI = 3.141549;
  * 
