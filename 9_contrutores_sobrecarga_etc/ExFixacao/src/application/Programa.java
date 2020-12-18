0f717f8fefadfe9e4f296641d6eb6f4d15189923
alexandre <alexandre.macedo1712@gmail.com>
2020-12-17 21:03:36 -0300

    Att ex de Fixacação resolvido

--- 9_contrutores_sobrecarga_etc/ExFixacao/src/application/Programa.java
+++ 9_contrutores_sobrecarga_etc/ExFixacao/src/application/Programa.java
@@ -5,8 +5,6 @@ import java.util.Scanner;
 
 import entities.Cliente;
 
-
-
 public class Programa {
 
 	public static void main(String[] args) {
@@ -20,14 +18,32 @@ public class Programa {
 		String name = sc.nextLine();
 
 		System.out.print("Is there na initial deposit (y/n)? ");
-		String resposta = sc.nextLine();
-		
+		char resposta = sc.next().charAt(0);
+
 		Cliente cliente = new Cliente(numberAcount, name);
 
-			if (resposta == "y")
-				System.out.print("Enter initial deposit value: ");
+		if (resposta == 'y') {
+			System.out.print("Enter initial deposit value: ");
+			cliente.saldo = sc.nextDouble();
+		}
+
 		// AGORA A PARTE DOS DADOS
+		System.out.println();
 		System.out.println("Account data: ");
+		System.out.printf("Account %s, Holder: %s, Balance: $ %.2f%n", numberAcount, name, cliente.saldo);
+		System.out.println();
+		
+		System.out.print("Enter a deposit value: ");
+		cliente.deposito = sc.nextDouble();
+		double x = cliente.deposit();
+		System.out.println("Updated account data: ");
+		System.out.printf("Account %s, Holder: %s, Balance: $ %.2f%n", numberAcount, name, cliente.saldo);
+		System.out.println();
+		
+		System.out.print("Enter a withdraw value: ");
+		cliente.saque = sc.nextDouble();
+		double y = cliente.saque();
+		System.out.println("Updated account data: ");
 		System.out.printf("Account %s, Holder: %s, Balance: $ %.2f", numberAcount, name, cliente.saldo);
 
 	}
--- 9_contrutores_sobrecarga_etc/ExFixacao/src/entities/Cliente.java
+++ 9_contrutores_sobrecarga_etc/ExFixacao/src/entities/Cliente.java
@@ -4,6 +4,8 @@ public class Cliente {
 	public String name;
 	public String numberAcount;
 	public double saldo;
+	public double deposito;
+	public double saque;
 
 	public Cliente() {
 	}
@@ -12,4 +14,13 @@ public class Cliente {
 		this.name = name;
 		this.numberAcount = numberAcount;
 	}
+
+	public double deposit() {
+		return saldo = saldo + deposito;
+	}
+
+	public double saque() {
+		return saldo =  (saldo - saque) - 5;
+	}
+
 }
